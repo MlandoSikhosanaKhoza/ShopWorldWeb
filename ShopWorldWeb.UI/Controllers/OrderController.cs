84435cd6e8950bcc64da6eca3a618ab8c3659603
mlando2.0 <mlando2.0@outlook.com>
2023-04-12 19:25:40 +0200

    Order Logic complete for controller

--- ShopWorldWeb.UI/Controllers/OrderController.cs
+++ ShopWorldWeb.UI/Controllers/OrderController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopWorld.Shared.Entities;
+using ShopWorldWeb.UI.Models;
+using ShopWorldWeb.UI.Services;
 
 namespace ShopWorldWeb.UI.Controllers
 {
     public class OrderController : Controller
     {
-        public IActionResult Index()
+        private ShopWorldClient _shopWorldClient;
+        public IMapper _mapper { get; set; }
+        public OrderController(ShopWorldClient shopWorldClient, IMapper mapper)
         {
-            return View();
+            _shopWorldClient = shopWorldClient;
+            _mapper = mapper;
+        }
+        public async  Task<IActionResult> Index()
+        {
+            ViewBag.OngoingOrders =await _shopWorldClient.Order_GetOngoingOrdersAsync();
+            ViewBag.CompleteOrders = await _shopWorldClient.Order_GetCompleteOrdersAsync();
+            return View("Index");
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Order order = await _shopWorldClient.Order_GetOrderAsync((int)id);
+            Customer customer = await _shopWorldClient.Customer_GetCustomerByIdAsync(order.CustomerId);
+            ViewBag.NameSurname = customer.Name + " " + customer.Surname;
+            List<OrderItemsViewModel> orderItems = (await _shopWorldClient.OrderItem_GetOrderViewItemsAsync(order.OrderId)).Select(oiv => new OrderItemsViewModel { Description = oiv.Description, Quantity = oiv.Quantity, Price = oiv.Price }).ToList();
+            OrderModel orderModel = _mapper.Map<OrderModel>(order);
+            orderModel.OrderItemsView = orderItems;
+            ViewBag.EmployeeId = new SelectList((await _shopWorldClient.Employee_GetAllEmployeesAsync()).Select(n => new { Id = n.EmployeeId, FullName = n.Name + " " + n.Surname }), "Id", "FullName", order.EmployeeId);
+            return View("Fulfill", orderModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> FulfillOrder(int OrderId, int EmployeeId)
+        {
+            Order order = await _shopWorldClient.Order_GetOrderAsync(OrderId);
+            order.EmployeeId = EmployeeId;
+            order.DateFulfilled = DateTime.Now;
+            await _shopWorldClient.Order_UpdateOrderAsync(order);
+            return RedirectToAction("Index");
         }
     }
 }
