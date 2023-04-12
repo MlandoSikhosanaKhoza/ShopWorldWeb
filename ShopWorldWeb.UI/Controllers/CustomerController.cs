0c6478fb8817deaa799fbbfd9c571ad453f21056
mlando2.0 <mlando2.0@outlook.com>
2023-04-12 02:21:13 +0200

    Add project files.

---
+++ ShopWorldWeb.UI/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopWorldWeb.UI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
