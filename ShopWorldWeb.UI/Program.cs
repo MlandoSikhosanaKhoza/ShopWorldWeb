81f7de39c0901276443adc8dde3150f523471daf
mlando2.0 <mlando2.0@outlook.com>
2023-04-12 02:38:19 +0200

    Cookie authentication Scheme added

--- ShopWorldWeb.UI/Program.cs
+++ ShopWorldWeb.UI/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
